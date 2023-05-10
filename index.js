5048cef14aa3dc0acb2de136677a4a0ba7e73700
Calin Buzetelu <calinbuze@gmail.com>
2023-05-10 21:25:00 +0200

    Update logic, and start npm script

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 'use strict';
 
-import { assert, log } from 'console';
-
-const stockWrapper = document.querySelector('.stock-container')
-const stockContainers = document.querySelectorAll('.stock');
 const welcomeContainer = document.querySelector('.welcome-page-container')
+const stockContainer = document.querySelector('.stock-container')
+const stocks = document.querySelectorAll('.stock');
 const submitButton = document.querySelector('.submit-button');
 
 const parseTickerCsv = async function () {
@@ -23,12 +21,12 @@ const parseTickerCsv = async function () {
 
 const generateMonkeyPortfolio = async function () {
     welcomeContainer.classList.add('hidden');
-    stockWrapper.classList.remove('hidden')
+    stockContainer.classList.remove('hidden')
     const allTickers = await parseTickerCsv();
     for (let i = 0; i < 10; i++) {
         const index = Math.floor(Math.random() * allTickers.length + 1);
         const ticker = allTickers[index];
-        stockContainers[i].textContent = ticker;
+        stocks[i].textContent = ticker;
     }
 }
 
